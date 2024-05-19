01a9878744b1fa0695268cd2047a66b4d4fb298d
INTEL <INTEL@DESKTOP-NV2M3DU>
2024-05-19 11:36:20 +0530

    my commit

--- src/mypack/myswaglab.java
+++ src/mypack/myswaglab.java
@@ -1,54 +1,39 @@
-package mypack;
-
+mypack;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+public class Openswaglabs {
 
-public class myswaglab {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","driver//chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.saucedemo.com/v1");
+		
+		driver.manage().window().maximize();
+		WebElement name= driver.findElement(By.id("user-name"));
+		name.sendKeys("standard_user");   //standard_user
 
-	public static void main(String[] args) throws InterruptedException  {
-		// TODO Auto-generated method stub
-		  System.setProperty("webdriver.chrome.driver", "Driver\\chromedriver.exe");
-	       // System.setProperty("webdriver.chrome.driver", "driver\\chromedriver.exe");
-	       WebDriver driver = new ChromeDriver();// open the browser
+		//name.submit();
+		//Thread.sleep(1000);
+		WebElement pass= driver.findElement(By.id("password"));
+		pass.sendKeys("secret_sauce");
+		//pass.submit();
+		
+		Thread.sleep(1000);
+		driver.findElement(By.id("login-button")).click();
+		
+		 //String currentwindowHandle1 = driver.getWindowHandle();
+	     //driver.switchTo().window(currentwindowHandle1);
+		
+		 driver.findElement(By.linkText("Sauce Labs Bolt T-Shirt")).click();
+		
+	   // WebElement add= driver.findElement(By.id("add-to-cart-sauce-labs-bolt-t-shirt"));
+	    //add.submit();
 
-	       driver.manage().window().maximize();// maximize the windows
-	       driver.get("https://www.saucedemo.com");
-	       WebElement username = driver.findElement(By.id("user-name"));
-	       WebElement password = driver.findElement(By.id("password"));
+	    
 
-	       Thread.sleep(1000);
-	       username.sendKeys("standard_user");
-	       Thread.sleep(1000);
-	       password.sendKeys("secret_sauce");
-
-	       WebElement Login = driver.findElement(By.id("login-button"));
-	       // id="login-button"
-	       Login.click();
-	       Thread.sleep(1000);
-	       // Switch to Product Page
-	       String currentwindowHandle = driver.getWindowHandle();
-	       driver.switchTo().window(currentwindowHandle);
-
-	       // Sauce Labs Bolt T-Shirt
-	       driver.findElement(By.linkText("Sauce Labs Bolt T-Shirt")).click();
-	       Thread.sleep(1000);
-
-	       // Switch to Product Page
-	       String currentwindowHandle1 = driver.getWindowHandle();
-	       driver.switchTo().window(currentwindowHandle1);
-
-	       // driver.findElement(By.className("btn_primary btn_inventory")).click();
-//	       driver.findElement(By.xpath("//button[text()='ADD TO CART']")).click();
-	  WebElement cart= driver.findElement(By.xpath("//*[@id=\"add-to-cart\"]"));
-	  cart.click();
-	  Thread.sleep(100);
-
-	       System.out.println("hi");
-	       
-	       //driver.close();
-	   }
+		
 	}
 
-
+}
